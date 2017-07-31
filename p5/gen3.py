f27da741494e46a0d35b360155bce4f8d0e15b70
cthbst <edison1998402@gmail.com>
2017-07-31 22:18:19 +0800

    p5 test data

---
+++ p5/gen3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from random import randint as ri
+import random
+import string    
+
+aph = "aaaaaaaaaaaaab"
+
+ncase = 5
+print(ncase)
+
+for _ in range(ncase):
+    n = ri(1,1000)
+    print(n)
+    x=ri(0,n-1)
+    y=ri(0,n-1)
+    d=ri(0,n);
+    
+    for i in range(0,n):
+        line = ""
+        for j in range(0,n):
+            dis = abs(i-x)+abs(j-y)
+            if dis>d :
+                line += random.choice(aph)
+            else :
+                line += aph[ (dis+1)*(abs(i-x)+1)%len(aph) ]
+        print(line)
