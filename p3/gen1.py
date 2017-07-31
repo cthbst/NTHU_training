c8862ae267ebcf980effa163de5bf649b550a5ba
cthbst <edison1998402@gmail.com>
2017-07-31 20:13:59 +0800

    p3 test data

---
+++ p3/gen1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from random import randint as ri
+
+MAXN = 100000
+MAXW = 10000
+
+ncase = 10
+print(ncase)
+
+for __ in range(ncase):
+    N=ri(1,MAXN/100)
+    M=ri(N,MAXN)
+    
+    print(N,M)
+    for _ in range(M):
+        print( ri(1,N) , ri(1,N) , ri(1,MAXW))
